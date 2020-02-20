ca761a56cad636b287b601fc6cfea5974b754be0
patrickyh <zyh3838438zyh@live.cn>
2020-02-20 12:06:46 -0800

    update main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,19 +16,19 @@ object Main {
         with MemoryExp
         with UncheckedHelper {
 
-        def readFile(filename: String): String = {
-          val source = scala.io.Source.fromFile(filename)
-          try source.getLines mkString "\n"
-          finally source.close()
-        }
+        // def readFile(filename: String): String = {
+        //   val source = scala.io.Source.fromFile(filename)
+        //   try source.getLines mkString "\n"
+        //   finally source.close()
+        // }
 
-        def parseProgram(prog: String): Program =
-          DlParser.parseAll(DlParser.program, prog).get
+        // def parseProgram(prog: String): Program =
+        //   DlParser.parseAll(DlParser.program, prog).get
 
         override def snippet(unused: Rep[Unit]) = {
-          val text = readFile("test/parser/simple_prog1.txt")
-          val prog = parseProgram(text)
-          new Interpreter(prog).run()
+          // val text = readFile("test/parser/simple_prog1.txt")
+          // val prog = parseProgram(text)
+          // new Interpreter(prog).run()
         }
       }
 
